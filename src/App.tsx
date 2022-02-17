12b749ab41eaa231a0beedd42a684eb011a7f6e4
Luiz Andrade <luizinhophe@hotmail.com>
2022-02-16 22:44:12 -0300

    main: vite

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,17 @@ import './App.css'
 import { useQuery } from 'urql';
 
 const HomePageQuery = `
-query {
-  pages {
-    logo {
-      fileName
-      url
+  query {
+    page (
+      where: { slug: "home" }
+    ) {
+      title
+      subtitle
+      logo {
+        url
+      }
     }
-    title
-    subtitle
   }
-}
-
 `
 function App() {
   const [count, setCount] = useState(0)
@@ -24,42 +24,18 @@ function App() {
   const { data, fetching, error } = result;
 
   useEffect(() => {
-    console.log(data)
-  }, [data])
+    console.log(data, result)
+  }, [fetching])
 
   if (fetching) return <p>Loading...</p>;
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
+        <img src={data.page.logo.url} className="App-logo" alt="logo" />
+        <p>{ data.page.title }</p>
         <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
+          { data.page.subtitle }
         </p>
       </header>
     </div>
