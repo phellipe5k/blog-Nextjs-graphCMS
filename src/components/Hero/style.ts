4840ce218975419d6b6f6b20f372de34b1043276
Luiz Andrade <luizinhophe@hotmail.com>
2022-02-19 22:10:26 -0300

    fixing routing path

--- src/components/Hero/style.ts
+++ src/components/Hero/style.ts
@@ -1,6 +1,6 @@
 import styled, { css } from 'styled-components';
 import { motion } from 'framer-motion';
-import { Theme } from 'themes/type';
+import { Theme } from '../../themes/type';
 
 export const Container = styled(motion.section)`
     ${ ({ theme } ) => {
